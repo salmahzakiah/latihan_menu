de05361aa5abb4bf054601e21f0f995e4d333954
RapiMaulana <kangrapi2@gmail.com>
2022-11-14 05:29:02 +0700

    latihan_menu

--- lib/menu/ketiga.dart
+++ lib/menu/ketiga.dart
@@ -11,7 +11,35 @@ class _KetigaPageState extends State<KetigaPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Halaman 3"),
+      child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[100],
+            child: const Text("Grid 1"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[400],
+            child: const Text("Grid 2"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[800],
+            child: const Text("Grid 3"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[900],
+            child: const Text("Grid 4"),
+          ),
+        ],
+      ),
     );
   }
 }
